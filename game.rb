b24cf4736262d04f936e7ad72a452015108fcb21
surajjayraman <suraj.jayraman@gmail.com>
2021-10-11 18:14:59 -0400

    Fix game logic: add method to switch players after a turn.

--- game.rb
+++ game.rb
@@ -5,19 +5,15 @@ class Game
   
     def initialize(player1, player2)
       @player1 = player1
-      @plater2 = player2
+      @player2 = player2
       @current_player = @player1
     end
   
-   def nextTurn
-      puts "----- NEW TURN -----"
-      if @current_player == self.player1
-        puts "prev turn was player1"
-        # puts player2.inspect
-        # self.current_player = self.player2
+    def switchPlayer
+      if self.current_player == self.player1
+        self.current_player = self.player2
       else
-        puts "prev turn was player2"
-        # self.current_player = self.player1
+        self.current_player = self.player1
       end 
     end
   
@@ -28,4 +24,4 @@ class Game
       exit(0)
     end
   
-  end 
+  end
--- main.rb
+++ main.rb
@@ -1,4 +1,39 @@
-# Main file that will require all other files
+require './player'
+require './question'
+require './game'
 
-# prob contain the game loop
-# use gets.chomp for inputs and puts for output
+player1 = Player.new(1)
+player2 = Player.new(2)
+
+game = Game.new(player1, player2)
+
+while true
+  # fire question
+  question = Question.new
+  puts "#{game.current_player.name}: #{question.question}"
+
+  # evaluate answer
+  print "> "
+  input = gets.chomp.to_i
+
+  if (input === question.answer)
+    puts "#{game.current_player.name}: YES! You are correct."
+  else 
+    puts "#{game.current_player.name}: Seriously? No!"
+    game.current_player.wrongAnswer
+  end
+
+  # move on to next round only if score is good
+  if player1.lives == 0 || player2.lives == 0
+    # switch to winner player
+    game.switchPlayer
+    # initial game shut down
+    game.gameOver
+  else
+    # move to next round
+    puts "P1: #{player1.lives}/3 vs P2: #{player2.lives}/3"
+    game.switchPlayer
+    puts "----- NEW TURN -----"
+  end
+
+end
